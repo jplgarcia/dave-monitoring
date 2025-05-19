c7dbe019aaf3aa1822aab21a33854a9d28790637
Fabio Oshiro <fabio.oshiro@calindra.com.br>
2025-05-19 20:19:53 -0300

    feat: fetch the currentSealedEpoch

--- canSettleMonitor.js
+++ canSettleMonitor.js
@@ -18,6 +18,34 @@ const abi = [
             { name: 'winnerCommitment', type: 'bytes32' }
         ],
     },
+    {
+        inputs: [],
+        name: "getCurrentSealedEpoch",
+        outputs: [
+            {
+                internalType: "uint256",
+                name: "epochNumber",
+                type: "uint256"
+            },
+            {
+                internalType: "uint256",
+                name: "inputIndexLowerBound",
+                type: "uint256"
+            },
+            {
+                internalType: "uint256",
+                name: "inputIndexUpperBound",
+                type: "uint256"
+            },
+            {
+                internalType: "contract ITournament",
+                name: "tournament",
+                type: "address"
+            }
+        ],
+        stateMutability: "view",
+        type: "function"
+    }
 ]
 
 const client = createPublicClient({
@@ -35,6 +63,20 @@ async function canSettle() {
         isFinished: result[0],
         epochNumber: (result[1] || 0).toString(),
         winnerCommitment: result[2],
+    }
+}
+
+async function getCurrentSealedEpoch() {
+    const result = await client.readContract({
+        address: DAVE_CONTRACT_ADDRESS,
+        abi: abi,
+        functionName: 'getCurrentSealedEpoch',
+    })
+    return {
+        epochNumber: (result[0] || -1).toString(),
+        inputIndexLowerBound: (result[1] || -1).toString(),
+        inputIndexUpperBound: (result[2] || -1).toString(),
+        tournament: result[3],
     }
 }
 
@@ -60,6 +102,14 @@ async function checkCanSettle() {
         res.lastCanSettleTimestamp = Date.now();
     } else {
         res.lastCanSettleTimestamp = data.lastCanSettleTimestamp || null;
+    }
+
+    const currentSealedEpoch = await getCurrentSealedEpoch()
+
+    if (currentSealedEpoch.epochNumber !== res.currentSealedEpoch?.epochNumber) {
+        res.currentSealedEpoch = { ...currentSealedEpoch, createdAt: Date.now() }
+    } else {
+        res.currentSealedEpoch = data.currentSealedEpoch
     }
 
     console.log(res)
